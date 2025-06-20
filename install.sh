05dff80a6e47d5babc4aca39bd3c7793d3a10b5f
Oscar Chua Wei Wen <152232264+odev-swe@users.noreply.github.com>
2025-06-21 06:45:29 +0800

    cleanup

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install chezmoi
-if ! command -v chezmoi >/dev/null; then
-    echo "Installing Chezmoi"
-    sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply git@github.com:odev-swe/dotfiles.git
-fi
-
-exit 0
